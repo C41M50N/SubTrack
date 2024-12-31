f44d2db4e6f367a69f0df194d74f15c2690ae834
Charles Buffington <dev.cfsb@proton.me>
2024-12-31 15:36:18 -0500

    fix production url maybe

--- src/features/auth/auth-client.ts
+++ src/features/auth/auth-client.ts
@@ -1,16 +1,5 @@
-import { env } from "@/env.mjs";
 import { createAuthClient } from "better-auth/react";
 
-export function getBaseUrl() {
-  return process.env.VERCEL_ENV === "production"
-    ? "https://www.subtrack.cbuff.dev"
-    : process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
-    : "http://localhost:3000";
-}
-
-export const authClient = createAuthClient({
-	baseURL: getBaseUrl(),
-});
+export const authClient = createAuthClient();
 
 export const { signIn, signOut, signUp, useSession } = authClient;
