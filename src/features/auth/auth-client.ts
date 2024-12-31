cf8aa7e8a15a86c0c5de625ae04c62d942d6222a
Charles Buffington <dev.cfsb@proton.me>
2024-12-31 15:30:11 -0500

    fix production url maybe

--- src/features/auth/auth-client.ts
+++ src/features/auth/auth-client.ts
@@ -1,6 +1,14 @@
 import { env } from "@/env.mjs";
 import { createAuthClient } from "better-auth/react";
 
+export function getBaseUrl() {
+  return process.env.VERCEL_ENV === "production"
+    ? "https://www.subtrack.cbuff.dev"
+    : process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
+    : "http://localhost:3000";
+}
+
 export const authClient = createAuthClient({
 	baseURL: window.location.origin,
 });
