8f466d4d99af6b48bdd4a5733120d7ed8ae2c961
Charles Buffington <dev.cfsb@proton.me>
2024-12-31 14:55:48 -0500

    fix production url maybe

--- src/features/auth/auth-client.ts
+++ src/features/auth/auth-client.ts
@@ -2,8 +2,8 @@ import { env } from "@/env.mjs";
 import { createAuthClient } from "better-auth/react";
 
 const BASE_URL =
-  process.env.NODE_ENV === "production"
-    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+  env.NODE_ENV === "production"
+    ? `https://${process.env.NEXT_PUBLIC_SITE_URL}`
     : "http://localhost:3000";
 
 export const authClient = createAuthClient({
