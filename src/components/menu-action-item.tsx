1938f8e95f19829ff6475971c6a84a13944712f1
Charles Buffington <dev.cfsb@proton.me>
2026-07-10 00:16:47 -0400

    Refactor MenuActionItem styles to use new chip color variables

--- src/components/menu-action-item.tsx
+++ src/components/menu-action-item.tsx
@@ -19,8 +19,8 @@ export function MenuActionItem({
         className={cn(
           'ring-1 rounded-md',
           variant === 'destructive'
-            ? 'bg-red-100 ring-red-300 dark:bg-red-950 dark:ring-red-800'
-            : 'bg-neutral-100 ring-neutral-300 dark:bg-neutral-800 dark:ring-neutral-700',
+            ? 'bg-chip-destructive ring-chip-destructive-border'
+            : 'bg-chip ring-chip-border',
         )}
       >
         <Icon className="p-1 size-4.5 stroke-3" />
