17c413017a884469d236f5ae4311a816daf49524
Charles Buffington <dev.cfsb@proton.me>
2026-07-27 23:54:27 -0400

    Configure invoice processing script and deployment branches

---
+++ src/jobs/process-due-invoices.ts
@@ -0,0 +1,6 @@
+export async function processDueInvoices() {
+  // TODO: Create invoices for due subscriptions and advance their next invoice dates.
+  console.log('Invoice processing is not implemented yet.');
+}
+
+await processDueInvoices();
